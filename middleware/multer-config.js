0c6779c639eebe9f77fcc5ca010c9cc0521b7adf
Mickael Brothier <mickaelbrothier@outlook.com>
2023-05-24 18:50:46 +0200

    Amélioration du mutler/sharp

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,45 +1,43 @@
 const multer = require('multer');
 const sharp = require('sharp');
-
-const MIME_TYPES = {
-  'image/jpg': 'jpg',
-  'image/jpeg': 'jpg',
-  'image/png': 'png'
-};
+const path = require('path');
 
 const storage = multer.memoryStorage();
 
-const multerConfig = {
-  upload: multer({
-    storage: storage,
-    fileFilter: (req, file, callback) => {
-      const isValidMimeType = Object.keys(MIME_TYPES).includes(file.mimetype);
-      const isValidExtension = isValidMimeType && MIME_TYPES[file.mimetype] === file.originalname.split('.').pop().toLowerCase();
-      isValidExtension ? callback(null, true) : callback(new Error('Invalid file type.'));
-    }
-  }).single('image'),
+const upload = multer({ storage }).single('image');
 
-  uploadImage: (req, res, next) => {
-    if (!req.file) {
-      return next();
-    }
+const uploadImage = async (req, res, next) => {
+  if (!req.file) {
+    return next();
+  }
 
-    const { buffer } = req.file;
-    const fileName = Date.now() + '.webp';
+  const { buffer, originalname } = req.file;
+  const { name, ext } = path.parse(originalname);
+  const fileName = `${Date.now()}-${name.replace(/\s/g, '_')}.webp`;
 
-    sharp(buffer)
+  const extensions = ['.png', '.jpg', '.jpeg'];
+  const fileExtension = ext.toLowerCase();
+
+  if (!extensions.includes(fileExtension)) {
+    const error = new Error('Le format d\'image n\'est pas pris en charge.');
+    error.status = 400;
+    return next(error);
+  }
+
+  try {
+    await sharp(buffer)
       .toFormat('webp')
       .webp({ quality: 5 })
-      .toFile('images/' + fileName)
-      .then(() => {
-        req.file.filename = fileName;
-        next();
-      })
-      .catch(error => {
-        console.error('Error occurred while converting image to WebP:', error);
-        next(error);
-      });
+      .toFile(`images/${fileName}`);
+
+    req.file.filename = fileName;
+    next();
+  } catch (error) {
+    console.error('Une erreur s\'est produite lors de l\'enregistrement de l\'image convertie:', error);
+    next(error);
   }
 };
 
-module.exports = multerConfig;
+module.exports = {  upload,  uploadImage};
+
+
