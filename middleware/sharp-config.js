b265c48368f543ba42b429400fd8498a1ab3b101
Mickael Brothier <mickaelbrothier@outlook.com>
2023-05-25 10:00:29 +0200

    Amélioration code sharp

--- middleware/sharp-config.js
+++ middleware/sharp-config.js
@@ -1,24 +1,28 @@
 const sharp = require('sharp');
+const path = require('path');
 
 const resizeImage = async (req, res, next) => {
-  if (!req.file) {
-    return next();
-  }
-
-  try {
+    if (!req.file) {
+      return next();
+    }
+  
     const { buffer, originalname } = req.file;
-
-    await sharp(buffer)
-      .toFormat('webp', { quality: 20 })
-      .toFile(`images/${originalname}.webp`);
-
-    req.file.filename = `${originalname}.webp`;
-    next();
-  } catch (error) {
-    console.error('Une erreur s\'est produite lors de l\'enregistrement de l\'image convertie:', error);
-    next(error);
-  }
-};
+    const { name, ext } = path.parse(originalname);
+    const fileName = `${name.replace(/\s/g, '_')}.webp`;
+  
+ 
+    try {
+      await sharp(buffer)
+        .toFormat('webp')
+        .webp({ quality: 20 })
+        .toFile(`images/${fileName}`);
+  
+      req.file.filename = fileName;
+      next();
+    } catch (error) {
+      console.error('Une erreur s\'est produite lors de l\'enregistrement de l\'image convertie:', error);
+      next(error);
+    }
+  };
 
 module.exports = resizeImage;
-
